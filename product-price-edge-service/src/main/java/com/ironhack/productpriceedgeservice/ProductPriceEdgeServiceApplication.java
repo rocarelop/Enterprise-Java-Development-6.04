91b1f4249380e01fc0487c9f15bbacdad7e5226b
Rocio Arellano López <rocioarellano_94@hotmail.com>
2022-07-05 17:59:29 +0200

    inicio ejercicio 2

---
+++ product-price-edge-service/src/main/java/com/ironhack/productpriceedgeservice/ProductPriceEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.productpriceedgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPriceEdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductPriceEdgeServiceApplication.class, args);
+	}
+
+}
